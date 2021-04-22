aa09355177ce4728659c27f6c9d1780caa72bcab
Pierluigi Barocci <pierluigi.barocci@nautes.com>
2021-04-22 14:08:55 +0200

    add Fonts and BorderRadius

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,9 @@ class MyApp extends StatelessWidget {
       title: 'MyShop',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
+        accentColor: Colors.deepOrange,
+        fontFamily: 'Lato',
       ),
       home: ProductsOverviewScreen(),
     );
